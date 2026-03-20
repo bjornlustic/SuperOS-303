6b2226b95a2bae7a8d7e54cbfd82b1919bfaf6f2
Nicholas J. Michalek <djphazer@gmail.com>
2026-03-20 11:49:21 -0400

    cleanup unused

--- makesyx.sh
+++
@@ -1,2 +0,0 @@
-python tools/hex2sysex.py .pio/build/app/firmware.hex > update.syx
-
--- merge_firmware.sh
+++
@@ -1,1 +0,0 @@
-~/.platformio/packages/tool-sreccat/srec_cat.exe .pio/build/app/firmware.hex -Intel .pio/build/bootloader/firmware.hex -Intel -o combined.hex -Intel
